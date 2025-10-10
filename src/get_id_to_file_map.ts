233b3bb29070032578f9a8dfc1eb37b57b925d4d
Alexander James Phillips <ajp.global@gmail.com>
2025-10-10 17:01:01 +0100

    Remove hard coded map for testing

--- src/get_id_to_file_map.ts
+++ src/get_id_to_file_map.ts
@@ -14,19 +14,6 @@ type GetIdToFileMapResult =
 }
 export async function get_id_to_file_map(supabase: SupabaseClient, id_and_version: IdAndVersion): Promise<GetIdToFileMapResult>
 {
-    if (2 > Math.random())
-    {
-        // Temporary hardcoded map for testing
-        return { map: new Map([
-            ["index.html", "1672acd0-bae1-422a-b115-93dcaeaf5c9e"],
-            ["script.js", "e8d33d4e-911c-4636-97fb-275e69a30ce6"],
-            ["style.css", "c2f40503-e8d8-4c78-95aa-6bfea2c680ae"],
-            ["assets/wikisim.png", "2e25fd96-efa9-48d2-a05b-191eda64c6ce"],
-            // Just to show we can access other fields
-            ["not used title", id_and_version.to_str()],
-        ]), error: null }
-    }
-
     const { component, error } = await get_component_by_id_and_version(id_and_version, supabase)
     if (error) return error_response(error.message, error.status_code)
 
